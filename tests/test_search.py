f2ec2aa6f470a1e96064ccb51b0a48950fb29edf
Pyotr Void <ptrpl4@ya.ru>
2021-12-20 22:06:58 +0300

    refactor fixture usage

--- tests/test_search.py
+++ tests/test_search.py
@@ -2,8 +2,10 @@ from pages.result import DuckDuckGoResultPage
 from pages.search import DuckDuckGoSearchPage
 import pytest
 
+queries = ['panda', 'python', 'dark souls']
 
-@pytest.mark.parametrize('phrase', ['panda', 'python', 'dark souls'])
+
+@pytest.mark.parametrize('phrase', [queries])
 def test_basic_duckduckgo_search(browser, phrase):
     search_page = DuckDuckGoSearchPage(browser)
     result_page = DuckDuckGoResultPage(browser)
@@ -11,10 +13,10 @@ def test_basic_duckduckgo_search(browser, phrase):
     search_page.load()
 
     search_page.search(phrase)
-    
+
     assert phrase == result_page.search_input_value()
 
     for title in result_page.result_link_titles():
         assert phrase.lower() in title.lower()
-    
+
     assert phrase in result_page.title()
