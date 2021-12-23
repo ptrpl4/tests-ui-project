8e5412655897d92e753f779d8f4e36d1f780f183
Pyotr Void <ptrpl4@ya.ru>
2021-12-23 15:22:23 +0300

    add exeption examples

--- tests/test_sandbox.py
+++ tests/test_sandbox.py
@@ -1,6 +1,19 @@
-# import pytest
+import pytest
 
-# def test_sandbox(browser):
-#     url='https://www.google.com'
-#     a = browser.get(url)
-#     assert a != False
+
+# --------------------------------------------------------------------------------
+# A test function that triggers an exception
+# --------------------------------------------------------------------------------
+def test_divide_by_zer0():
+    num = 1 / 0
+    return num
+
+
+# --------------------------------------------------------------------------------
+# A test function that verifies an exception
+# --------------------------------------------------------------------------------
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError) as e:
+        num = 1 / 0
+
+    assert 'division by zero' in str(e.value)
